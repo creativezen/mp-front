221699b835ab4ff75ea1b40685cd190efbd7aa31
creativezen <tocha2782@gmail.com>
2022-11-28 10:23:19 +0300

    added .prettierrc

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,50 +1,48 @@
-import { useEffect, useState } from 'react'
-import { Helmet } from 'react-helmet'
+import { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
 
-import { useAppSelector } from 'store'
-import { get } from 'helpers/request'
-import ProductCard from 'blocks/ProductCard'
-import { selectFavorites } from 'features/Favorites/selectors'
-import { PageWrapper } from 'App.styled'
-import { ProductGroup, ProductGroupContainer } from './styled'
-import type { I_UniRes } from 'types'
-
+import { useAppSelector } from "store";
+import { get } from "helpers/request";
+import ProductCard from "blocks/ProductCard";
+import { selectFavorites } from "features/Favorites/selectors";
+import { PageWrapper } from "App.styled";
+import { ProductGroup, ProductGroupContainer } from "./styled";
+import type { I_UniRes } from "types";
 
 const HomePage: React.FC = () => {
-  const idsInFavorites = useAppSelector(selectFavorites)
+  const idsInFavorites = useAppSelector(selectFavorites);
 
-  const [ products, setProducts ] = useState<any[]>()
+  const [products, setProducts] = useState<any[]>();
 
   useEffect(() => {
-    get('/products')
-      .then((res: I_UniRes) => setProducts(res.data))
-  }, [])
+    get("/products").then((res: I_UniRes) => setProducts(res.data));
+  }, []);
 
+  if (!products) return <p>Loading</p>;
 
-  if (!products) return <p>Loading</p>
+  return (
+    <>
+      <Helmet>
+        <title>Главная - MW Marketplace</title>
+      </Helmet>
 
+      <PageWrapper>
+        <ProductGroup>
+          <h2>Рекомендуемые товары</h2>
 
-  return <>
-    <Helmet>
-      <title>Главная - MW Marketplace</title>
-    </Helmet>
+          <ProductGroupContainer>
+            {products.map((product) => (
+              <ProductCard
+                {...product}
+                key={product.id}
+                isLiked={idsInFavorites.includes(product.id)}
+              />
+            ))}
+          </ProductGroupContainer>
+        </ProductGroup>
+      </PageWrapper>
+    </>
+  );
+};
 
-    <PageWrapper>
-      <ProductGroup>
-        <h2>Рекомендуемые товары</h2>
-
-        <ProductGroupContainer>
-          {products.map((p) => (
-            <ProductCard
-              {...p}
-              key={p.id}
-              isLiked={idsInFavorites.includes(p.id)}
-            />
-          ))}
-        </ProductGroupContainer>
-      </ProductGroup>
-    </PageWrapper>
-  </>
-}
-
-export default HomePage
+export default HomePage;
